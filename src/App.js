129972152378b03ce845b99c299dbe980be7b199
Tiffani <tiffani.grace.elmore@gmail.com>
2017-11-13 18:32:13 -0600

    corrected homepage path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Homepage from "./views/Homepage"
+import Homepage from "../src/views/Homepage"
 
 class App extends Component {
   render() {
